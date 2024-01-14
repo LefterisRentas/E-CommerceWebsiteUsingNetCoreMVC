30a794e8b762a85e34416981095db7ad7d88e545
Lefteris <std161088@ac.eap.gr>
2024-01-14 15:36:46 +0200

    Features

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -30,9 +30,70 @@ namespace E_CommerceWebsiteUsingNetCoreMVC.Controllers
             {
                 _db.Categories.Add(category);
                 _db.SaveChanges();
+                TempData["success_created"] = "The Category Has Been Created Successfully";
                 return RedirectToAction("Index");
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success_edited"] = "The Category Has Been Edited Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? cat = _db.Categories.Find(id);
+            if (cat == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(cat);
+            _db.SaveChanges();
+            TempData["success_deleted"] = "The Category Has Been Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+
     }
 }
